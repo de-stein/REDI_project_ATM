9265f97198b16c9231a20876e6978dd42a52a8b2
Dzianis <81986373+de-stein@users.noreply.github.com>
2022-05-08 18:08:54 +0200

    Update REDI_Python_Project_ATM.py

--- REDI_Python_Project_ATM.py
+++ REDI_Python_Project_ATM.py
@@ -1,19 +1,17 @@
 print('Welcome to Srarkasse')
 trying = 3
-realpassword = 5555
-while trying !=0:
-password = int(input('Enter your Password'))
-if password != realpassword:
-    trying = trying - 1 
-    print('invalid password! You have {}  tries left.' .format(trying) )
-        else:
-            user_choise = input('pay out cash? y/n')
+true_pin = 5555
+while trying != 0:
+    pin = int(input('Enter your Password'))
+    if pin != true_pin:
+        trying = trying - 1 
+        print('invalid password! You have {}  tries left.' .format(trying) )
+    else:
+        user_choise = input('pay out cash? y/n')
         if user_choise == 'y':
             user_sum = input('Enter the Amount')
-        print('Here please, your {} €' .format(user_sum))    
-         if user_choise == 'n': 
+            print('Here please, your {} €' .format(user_sum))
+        if user_choise == 'n': 
              user_exit = input('would you like to exit? y/n')
              print("thank you for using Sparkasse!")
         break 
-    else:
-        continue
