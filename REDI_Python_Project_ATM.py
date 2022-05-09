980723784c753a31d9b202b588e30f3ca7a2b472
Dzianis <81986373+de-stein@users.noreply.github.com>
2022-05-09 16:14:35 +0200

    Update REDI_Python_Project_ATM.py

--- REDI_Python_Project_ATM.py
+++ REDI_Python_Project_ATM.py
@@ -1,21 +1,25 @@
 print('Welcome to Srarkasse')
 trying = 3
 true_pin = 5555
+enter_your_chois = 'Enter "d" to deposit, enter "w" to withdraw, enter "e" to exit'
+goodbye = "Thank you for using Sparkasse!"
 while trying != 0:
     pin = int(input('Enter your Password'))
     if pin != true_pin:
         trying = trying - 1
         print('invalid password! You have {} tries left.' .format(trying) )
     else:
-        user_choise = input('Enter "d" to deposit, enter "w" to withdraw, enter "e" to exit')
+        user_choise = input(enter_your_chois)
         if user_choise == 'd':
             deposit = input('Enter the Amount')
             print('Here please, your amount deposited is: {} €' .format(deposit))
+            print(goodbye)
         elif user_choise == 'w':
             withdraw = input('Enter the Amount, put the money and press Enter')
-            print('Here please, your amount withdrawn is: {} €' .format(withdraw))
+            print('Here please, your amount withdrawn is: {} €'.format(withdraw))
+            print(goodbye)
         elif user_choise == 'e':
-                print("Thank you for using Sparkasse!")
+                print(goodbye)
         else:
-            print("Thank you for using Sparkasse!")
+            print(goodbye)
         break
